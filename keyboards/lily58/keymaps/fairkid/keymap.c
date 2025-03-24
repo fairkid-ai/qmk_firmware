467d5da62d5e6713cf77893bf67bf14e9de996b8
Fairkid <info@fairkid.ca>
2025-03-24 00:07:09 -0700

    Fairkid: fairkid keymap works

--- keyboards/lily58/keymaps/fairkid/keymap.c
+++ keyboards/lily58/keymaps/fairkid/keymap.c
@@ -1,14 +1,10 @@
 #include QMK_KEYBOARD_H
-#include "version.h"
-#include "features/achordion.h"
-#include "features/swapper.h"
-#include "features/num_word.h"
 
 enum layers {
-    LAYERS_BASE = 0,
-    LAYERS_LFN,
-    LAYERS_RFN,
-    LAYERS_MEDIA,
+    L_BASE = 0,
+    L_LFN,
+    L_RFN,
+    L_MEDIA,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,12 +24,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 
-[LAYERS_BASE] = LAYOUT(
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPC,
-    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_EQL,
-    KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
-    KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     KC_LBRC,   KC_RBRC,  KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_MINS,
-                      MO(LAYERS_LFN),   KC_LGUI, KC_LALT,  KC_SPC,    KC_ENT,   KC_SPC,  KC_APP,  MO(LAYERS_RFN)
+[L_BASE] = LAYOUT(
+    KC_GRV,   KC_1,   KC_2,    KC_3,       KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPC,
+    KC_TAB,   KC_Q,   KC_W,    KC_E,       KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_EQL,
+    KC_LSFT,  KC_A,   KC_S,    KC_D,       KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
+    KC_LCTL,  KC_Z,   KC_X,    KC_C,       KC_V,    KC_B,     KC_LBRC,   KC_RBRC,  KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_MINS,
+                               MO(L_LFN),  KC_LGUI, KC_LALT,  KC_SPC,    KC_ENT,   KC_SPC,  KC_APP,  MO(L_RFN)
 ),
 
 /* LEFT FUNCTION
@@ -50,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
- [LAYERS_LFN] = LAYOUT(
+ [L_LFN] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,   KC_DEL,
     _______,  KC_F11,   KC_F12,   _______,  _______,  _______,                      _______,  _______,  KC_PSCR,  KC_INS,    KC_HOME,  KC_PGUP,
     KC_CAPS,  _______,  _______,  _______,  _______,  _______,                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_END,   KC_PGDN,
@@ -73,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 
-[LAYERS_RFN] = LAYOUT(
+[L_RFN] = LAYOUT(
     KC_ESC,   _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  KC_UP,    _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
     KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
@@ -94,17 +90,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-[LAYERS_MEDIA] = LAYOUT(
+[L_MEDIA] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                _______, _______, _______, _______, _______, _______, _______, _______
-)
+),
 };
 
-void matrix_scan_user(void) {
-    achordion_task();
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, L_LFN, L_RFN, L_MEDIA);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
